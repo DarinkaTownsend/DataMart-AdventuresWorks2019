29db389ec59f4cb445c5f457d12c93a2aac14438
darinka townsend <darinka.townsend@hotmail.com>
2023-08-25 21:30:10 -0500

    Interfaz grafica

--- DashBoard/main.py
+++ DashBoard/main.py
@@ -7,82 +7,75 @@ import boto3
 
 warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Ventas AdventureWorks",page_icon=":bar_chart:",layout="wide")
-st.title(" :bar_chart: DashBoard - Ventas AdventureWorks ")
+st.title(" :bar_chart: DashBoard AdventureWorks ")
 st.markdown("<style>div.block-container{padding-top:1rem;}</style>",unsafe_allow_html=True)
-fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
-if fl is not None:
-    filename = fl.name
-    st.write(filename)
-    df=pd.read_csv(filename,on_bad_lines='skip',encoding="ISO-8859-1")
-else:
-    os.chdir(r"..\DashBoard")
-    df=pd.read_csv("..\DashBoard\Superstore7.csv",on_bad_lines='skip',encoding="ISO-8859-1")
 
-col1, col2 = st.columns((2))
-df["Order Date"] = pd.to_datetime(df["Order Date"])
 
-# Getting the min and max date
-startDate = pd.to_datetime(df["Order Date"]).min()
-endDate = pd.to_datetime(df["Order Date"]).max()
-
-with col1:
-    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
-
-with col2:
-    date2 = pd.to_datetime(st.date_input("End Date", endDate))
-
-df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+os.chdir(r"..\DashBoard")
+df=pd.read_csv("..\DashBoard\FACT-AdventureWorks.csv",on_bad_lines='skip',encoding="ISO-8859-1")
+df2=df.copy()
+df3=df.copy()
 
 st.sidebar.header("Choose your filter: ")
-# Create for Region
-region = st.sidebar.multiselect("Pick your Region", df["Region"].unique())
-if not region:
-    df2 = df.copy()
-else:
-    df2 = df[df["Region"].isin(region)]
 
-# Create for State
-state = st.sidebar.multiselect("Pick the State", df2["State"].unique())
-if not state:
-    df3 = df2.copy()
-else:
-    df3 = df2[df2["State"].isin(state)]
-
-# Create for City
-city = st.sidebar.multiselect("Pick the City",df3["City"].unique())
-
-# Filter the data based on Region, State and City
-
-if not region and not state and not city:
-    filtered_df = df
-elif not state and not city:
-    filtered_df = df[df["Region"].isin(region)]
-elif not region and not city:
-    filtered_df = df[df["State"].isin(state)]
-elif state and city:
-    filtered_df = df3[df["State"].isin(state) & df3["City"].isin(city)]
-elif region and city:
-    filtered_df = df3[df["Region"].isin(region) & df3["City"].isin(city)]
-elif region and state:
-    filtered_df = df3[df["Region"].isin(region) & df3["State"].isin(state)]
-elif city:
-    filtered_df = df3[df3["City"].isin(city)]
-else:
-    filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
-
-category_df = filtered_df.groupby(by=["Category"], as_index=False)["Sales"].sum()
-
+territorio = st.sidebar.multiselect("Selecciona el territorio", df["TerritoryName"].unique())
+st.subheader("Ventas totales realizadas por año por territorio")
+col1,col2=st.columns((2))
+filtered_df = df[df["TerritoryName"].isin(territorio)]
 with col1:
-    st.subheader("Category wise Sales")
-    fig = px.bar(category_df, x="Category", y="Sales", text=['${:,.2f}'.format(x) for x in category_df["Sales"]],
+    category_df = filtered_df.groupby(by=["Anio"], as_index=False)["TotalDue"].sum()
+    fig = px.bar(category_df, x="Anio", y="TotalDue", text=['${:,.0f}'.format(x) for x in category_df["Anio"]],
                  template="seaborn")
     st.plotly_chart(fig, use_container_width=True, height=200)
-
+    fig.update_traces(text=filtered_df["TerritoryName"], textposition="outside")
 with col2:
-    st.subheader("Region wise Sales")
-    fig = px.pie(filtered_df, values="Sales", names="Region", hole=0.5)
-    fig.update_traces(text=filtered_df["Region"], textposition="outside")
+    fig = px.pie(filtered_df, values="TotalDue", names="TerritoryName", hole=0.5)
+    fig.update_traces(text=filtered_df["TerritoryName"], textposition="outside")
     st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+country = st.sidebar.multiselect("Selecciona el país",df["Country"].unique())
+st.subheader("Ventas totales realizadas por año por país")
+col1,col2=st.columns((2))
+filtered_df3 = df3[df3["Country"].isin(country)]
+with col1:
+    category_df3 = filtered_df3.groupby(by=["Anio"], as_index=False)["TotalDue"].sum()
+    fig3 = px.bar(category_df3, x="Anio", y="TotalDue", text=['${:,.0f}'.format(x) for x in category_df3["Anio"]],
+                 template="seaborn")
+    st.plotly_chart(fig3, use_container_width=True, height=200)
+    fig3.update_traces(text=filtered_df3["Country"], textposition="outside")
+with col2:
+    fig3 = px.pie(filtered_df3, values="TotalDue", names="Country", hole=0.5)
+    fig3.update_traces(text=filtered_df3["Country"], textposition="outside")
+    st.plotly_chart(fig3, use_container_width=True)
+
+
+
+
+
+
+
+
+
+Continent = st.sidebar.multiselect("Selecciona el continente", df2["Continent"].unique())
+st.subheader("Ventas totales realizadas por año por continente")
+col1,col2=st.columns((2))
+filtered_df2 = df2[df2["Continent"].isin(Continent)]
+with col1:
+    category_df2 = filtered_df2.groupby(by=["Anio"], as_index=False)["TotalDue"].sum()
+    fig2 = px.bar(category_df2, x="Anio", y="TotalDue", text=['${:,.0f}'.format(x) for x in category_df2["Anio"]],
+                 template="seaborn")
+    st.plotly_chart(fig2, use_container_width=True, height=200)
+    fig2.update_traces(text=filtered_df2["Continent"], textposition="outside")
+with col2:
+    fig2 = px.pie(filtered_df2, values="TotalDue", names="Continent", hole=0.5)
+    fig2.update_traces(text=filtered_df2["Continent"], textposition="outside")
+    st.plotly_chart(fig2, use_container_width=True)
+
+
+
 
 
 
