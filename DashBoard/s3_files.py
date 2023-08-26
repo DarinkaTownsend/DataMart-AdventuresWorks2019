8f00bff5f1dd9f4773116c6a528d027c197aa730
darinka townsend <darinka.townsend@hotmail.com>
2023-08-25 22:06:09 -0500

    commit

--- DashBoard/s3_files.py
+++ DashBoard/s3_files.py
@@ -7,14 +7,3 @@ def download_s3_object(bucket, object, save_as):
     
     s3_client.download_file(bucket, object, save_as)
     
-
-"""
-from s3_files import download_s3_object
-...
-# Guarda el .csv de la tabla de hechos 
-bucket = 'adventuretransformado'
-object = 'FACT-AdventureWorks_part00000.csv'
-save_as = 'FACT-AdventureWorks.csv'
-
-download_s3_object(bucket, object, save_as)
-"""
